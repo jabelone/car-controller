6f65dfeba42d5f9bf4c03163af501a9c85ef4125
Jaimyn Mayer <jabelone@users.noreply.github.com>
2016-06-27 16:48:59 +1000

    Reverse support and fixed auto bug
    Added reverse support in manual mode.
    Hopefully fixed a bug that sometimes corrupted input values.  (so the resulting outputs were incorrect)

--- car_controller/car_controller.ino
+++ car_controller/car_controller.ino
@@ -7,6 +7,7 @@ const int dirInitial = true; // Set the initial direction; forwards/backwards de
 const int modeThreshold = 1200;
 const int killThreshold = 1600;
 const int noSignalThreshold = 500;
+const int reverseThreshold = 1340;
 
 //Define pin numbers
 const int inSteer = 11; // Pin connected to steering output on R/C receiver
@@ -39,18 +40,18 @@ void setup() {
   pinMode(inMode, INPUT); //voltage sensing pin
   pinMode(inMotor, INPUT); //voltage sensing pin
   pinMode(inSteer, INPUT); //voltage sensing pin
-  
+
   steering.attach(steerPin);
 
   //Set the default outputs
   digitalWrite(dirPin, dirInitial); //set the initial direction
-  
+
   straightAndStop();
 }
 
 void loop() {
   rxMode = pulseIn(inMode, HIGH, 20000); // Read the mode output on R/C receiver
-  
+
   if (millis() % 500 == 0) { //don't spam the console
 
     voltage = map(analogRead(voltSensePin), 0, 1023, 0, 480);
@@ -61,7 +62,7 @@ void loop() {
     LEDstatus = !LEDstatus; //Flip the status for next time this runs
     digitalWrite(ledPin, LEDstatus); //turn the LED on or off
   }
-  
+
   if (rxMode < noSignalThreshold) {
     straightAndStop();
   }
@@ -72,25 +73,39 @@ void loop() {
   }
 
   else if (rxMode < modeThreshold) {  //Manual mode
-    rxSteer = map(pulseIn(inSteer, HIGH, 20000), 800, 2000, 50, 150); // Read the steering output on R/C receiver
-    rxMotor = map(pulseIn(inMotor, HIGH, 20000), 820, 2000, 0, 200); // Read the motor output on R/C receiver
-    
+
     if (mode == true) { // If we just came from auto control
       straightAndStop(); // Set the motor power to zero and straighten wheels
       Serial.println("Entered manual control mode.");
       mode = false; // Now we're in manual mode
     }
 
-    if (millis() % 20 == 0) {
-    if (debug == true) {
-      Serial.print("rxSteer: "); // Read the mode output on R/C receiver);
-      Serial.print(rxSteer);
-      Serial.print("  rxMotor: ");
-      Serial.println(rxMotor);
-      Serial.print("  rxMode: ");
-      Serial.println(rxMode);
-    }}
+    rxSteer = map(pulseIn(inSteer, HIGH, 20000), 900, 1900, 50, 150); // Read the steering output on R/C receiver
+    rxMotor = pulseIn(inMotor, HIGH, 20000);
     
+    if (rxMotor < reverseThreshold) {
+      digitalWrite(dirPin, false);
+      rxMotor = map(rxMotor, reverseThreshold, 900, 0, 200); // Read the motor output on R/C receiver
+    }
+
+    else {
+      digitalWrite(dirPin, true);
+      rxMotor = map(rxMotor, reverseThreshold, 1900, 0, 200); // Read the motor output on R/C receiver
+    }
+
+
+
+    if (millis() % 20 == 0) {
+      if (debug == true) {
+        Serial.print("rxSteer: "); // Read the mode output on R/C receiver);
+        Serial.print(rxSteer);
+        Serial.print("  rxMotor: ");
+        Serial.println(rxMotor);
+        Serial.print("  rxMode: ");
+        Serial.println(rxMode);
+      }
+    }
+
     analogWrite(motorPin, rxMotor);
     steering.write(rxSteer);
   }
@@ -100,18 +115,16 @@ void loop() {
       straightAndStop(); // Set the motor power to zero and straighten wheels
       Serial.println("Entered autonomous control mode.");
     }
-    
+
     mode = true; // Now we're in auto mode
 
-    while (Serial.available() > 0) { //If there is something in the serial buffer waiting to be read
-
+      while (Serial.available() > 0) { //If there is something in the serial buffer waiting to be read
+      //Serial.println(Serial.readStringUntil('\n'));
       command = Serial.readStringUntil(',');
-      Serial.read(); //skip the comma
       value = Serial.readStringUntil('\n');
       Serial.read(); //skip the new line
 
-      if (debug == true) Serial.println("Set the " + command + " to " + value);
-
+      if (debug == true) Serial.println("Set the " + command + " to " + value.toInt());
 
       if (command == "steer") {
         steering.write(value.toInt());
@@ -136,5 +149,6 @@ void straightAndStop() {
   analogWrite(motorPin, 0);
   steering.write(100);
 }
+
 
 
